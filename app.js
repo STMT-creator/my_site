2bba992bf54305c4a249f71bf903512ace5f4918
STMT <elahep113@naver.com>
2024-12-02 17:19:33 +0900

    mongodb dotenv 추가

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config_db/database.js");
 const expressLayouts = require('express-ejs-layouts');
 const mainRouter = require("./routes/mainRouter.js")
+
+connectDB();
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
