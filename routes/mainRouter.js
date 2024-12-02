6afcec7f403530c31288bc87be41cb355da5559c
STMT <elahep113@naver.com>
2024-12-02 16:26:20 +0900

    메인 라우터 | about, contact

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router();
 const mainLayout = "../views/layouts/common.ejs";
 
+
+/**
+ * 언어별 주석처리 및 기능설명  --> 별포 두개로 시작
+ * 첫 페이지 : index router
+ * GET / or /home
+ */
 router.get('/', (req, res) => {
-  // res.render('index', {
-  //   layout: mainLayout,
-  //   title: 'My Site',
-  //   header: 'Special Header'
-  // });
   const locals = {
     title: "Beginner STMT",
     header: "Web Developer's Tip/Knowhow",
@@ -18,8 +19,37 @@ router.get('/', (req, res) => {
   });
 });
 
+
+/**
+ * About : 사이트 소개
+ * GET /about
+ */
+router.get('/about', (req, res) => {
+  const locals = {
+    title: 'about page',
+    header: "Web Developer's Tip/Knowhow"
+  };
+  res.render("about", {
+    locals: locals,
+    layout: "layouts/common"
+  })
+});
+/**
+ * Contact : 작업 요청, 의뢰
+ * GET /contact
+ */
+router.get('/contact', (req, res) => {
+  const locals = {
+    title: 'contact page',
+    header: "Web Developer's Tip/Knowhow"
+  };
+  res.render("contact", {
+    locals: locals,
+    layout: "layouts/common"
+  })
+});
+
 router.get('/signup', (req, res) => {
-// res.send('Hello World!')
 res.render('join');
 });
 
