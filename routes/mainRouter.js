f469e7c46f3edfa02ffab3c0c967040a0c3d60b1
STMT <elahep113@naver.com>
2024-12-04 16:12:34 +0900

    adminRouter 분리

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -53,20 +53,4 @@ router.get('/signup', (req, res) => {
   res.render('join');
 });
 
-/**
- * 관리자 로그인 요청
- * GET /admin
- */
-router.get('/admin', (req, res) => {
-  const locals = {
-    title: "STMT",
-    header: "관리자 로그인",
-    //res.send('admin login');
-  }
-  res.render("admin/index", {
-    locals: locals,
-    layout: "layouts/common"
-  })
-})
-
 module.exports = router;
