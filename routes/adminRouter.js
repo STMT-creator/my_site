ce36972c36ec0db3ff6a2be1493c38401009b815
STMT <elahep113@naver.com>
2024-12-04 17:27:10 +0900

    login | input <=> database 비교

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt")
 const mainLayout = "../views/layouts/common.ejs"
 const User = require("../models/User.js");
 
 
 /**
- * 관리자 로그인 요청
+ * 관리자 로그인 폼
  * GET /admin
  */
 router.get('/admin', (req, res) => {
     const locals = {
-      title: "STMT Admin",
-      header: "관리자 로그인",
+        title: "STMT Admin",
+        header: "관리자 로그인",
     };
-    res.render("admin/index", { locals, layout: mainLayout});
-  })
-  
+    res.render("admin/index", { locals, layout: mainLayout });
+})
+
+/**
+ * 관리자 로그인 요청
+ * POST /admin
+ */
+router.post('/admin', async (req, res) => {
+    const locals = {
+        title: "STMT Admin",
+        header: "관리자 로그인",
+    };
+    const { admin_id, admin_pwd } = req.body;
+    console.log(admin_id, admin_pwd);
+    const user = await User.find({ user_id: admin_id })
+    // user_id가 admin_id랑 같은걸 매칭
+    // 상수의 admin_id는 입력값이 admin_id이기 때문에
+    // 그것으로 넘기고 비교는 user_id
+
+    if (!user) {
+        throw new Error('user account is not found')
+    }
+    console.log(user);
+    res.render("admin/index", { locals, layout: mainLayout });
+})
+
+
 /**
  * 회원 가입 폼 보기
  * GET /register 
@@ -28,8 +53,29 @@ router.get('/register', (req, res) => {
  * 회원 가입 요청
  * POST /register
 */
-router.post('/register', (req, res) => {
-    res.send('회원 가입 폼의 데이터를 서버로 전송합니다.')
+router.post('/register', async (req, res) => {
+    try {
+        const { user_id, user_nick, user_pwd, user_email } = req.body;
+        const hashedPwd = await bcrypt.hash(user_pwd, 10);
+        const user = User({
+            user_id: user_id,
+            user_pwd: hashedPwd,
+            user_nick: user_pwd,
+            user_email: user_email
+        });
+        const savedUser = await user.save();
+
+        if (!user) {
+            throw new Error('계정 생성 실패!')
+        }
+        res.status(200).send({
+            message: "user account has created",
+            data: user
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 /**
@@ -40,4 +86,4 @@ router.post('/find', (req, res) => {
     res.send('이름 또는 이메일정보로 회원 ID/PW 찾기')
 })
 
-  module.exports = router;
+module.exports = router;
