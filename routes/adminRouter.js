b34fc7971881f898c9ad8c726783d061d4c68b03
STMT <elahep113@naver.com>
2024-12-06 17:18:21 +0900

    admin log in & post page UI base

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET; // env 변수 값 가져오기
+const cookieParser = require("cookie-parser");
 const mainLayout = "../views/layouts/common.ejs"
 const User = require("../models/User.js");
 
@@ -22,24 +25,51 @@ router.get('/admin', (req, res) => {
  * POST /admin
  */
 router.post('/admin', async (req, res) => {
-    const locals = {
-        title: "STMT Admin",
-        header: "관리자 로그인",
-    };
-    const { admin_id, admin_pwd } = req.body;
-    console.log(admin_id, admin_pwd);
-    const user = await User.find({ user_id: admin_id })
-    // user_id가 admin_id랑 같은걸 매칭
-    // 상수의 admin_id는 입력값이 admin_id이기 때문에
-    // 그것으로 넘기고 비교는 user_id
+    // const locals = {
+    //     title: "STMT Admin",
+    //     header: "관리자 로그인",
+    // };
+    try {
+        const { admin_id, admin_pwd } = req.body;
 
-    if (!user) {
-        throw new Error('user account is not found')
-    }
-    console.log(user);
-    res.render("admin/index", { locals, layout: mainLayout });
+        console.log(admin_id, admin_pwd);
+
+        const user = await User.find({ user_id: admin_id })
+        // user_id가 admin_id랑 같은걸 매칭
+        // 상수의 admin_id는 입력값이 admin_id이기 때문에
+        // 그것으로 넘기고 비교는 user_id
+        if (!user) {
+            throw new Error('user account is not found')
+        }
+
+        const isValid = bcrypt.compare(admin_pwd, user.user_pwd);
+        console.log(isValid)
+        if (!isValid) {
+            throw new Error('user password is not matched!')
+        }
+        const token = jwt.sign({ id: user.admin_id }, jwtSecret);
+        res.cookie("x_auth", token, { httpOnly: true })
+        res.redirect('/allPosts')
+    } catch (err) {
+        console.log(err)
+    } 
 })
 
+/**
+ * 관리자 전용 페이지 : 작성한 게시물 목록 화면 + 로그아웃 [버튼]
+ * GET /allPosts
+*/
+router.get('/allPosts', (req, res) => {
+    const locals = {
+        title: "STMT Admin",
+        header: "관리 페이지",
+    };
+    res.render("admin/allPosts", { locals, layout: mainLayout })
+})
+// const token = req.cookies.token;
+// if (!token) {
+//     res.redirect('/admin');
+// }
 
 /**
  * 회원 가입 폼 보기
@@ -50,7 +80,7 @@ router.get('/register', (req, res) => {
 })
 
 /**
- * 회원 가입 요청
+ * 관리자 회원 가입 요청
  * POST /register
 */
 router.post('/register', async (req, res) => {
@@ -60,7 +90,7 @@ router.post('/register', async (req, res) => {
         const user = User({
             user_id: user_id,
             user_pwd: hashedPwd,
-            user_nick: user_pwd,
+            user_nick: user_nick,
             user_email: user_email
         });
         const savedUser = await user.save();
