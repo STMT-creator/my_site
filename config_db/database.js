dbcab3d7faebac08767bdb6f4b3c6836941f0399
STMT <elahep113@naver.com>
2024-12-06 17:15:41 +0900

    서버 유지를 위한 디버깅 모듈

--- config_db/database.js
+++ config_db/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+mongoose.set('debug', true);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
@@ -9,6 +9,7 @@ const connectDB = async () => {
         if (!conn) {
             console.log('==================== DB 연결 실패 ====================')
         }
+        // console.log(conn.connection.host, conn.connection.name)
     }
     catch (err) {
         console.log('==================== DB 연결 에러 ====================')
