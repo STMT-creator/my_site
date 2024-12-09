dfefe6a15b5a67f3f73afebb15f04f635953da72
STMT <elahep113@naver.com>
2024-12-09 16:49:25 +0900

    post 스키마 생성

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const postSchema = new mongoose.Schema ({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+const Post = mongoose.model("Post", postSchema)
+
+module.exports = Post;
