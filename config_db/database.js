4fc136cfee28eb486933fefab5ac521a9a21f842
STMT <elahep113@naver.com>
2024-12-09 16:49:12 +0900

    디버그 콘솔 주석

--- config_db/database.js
+++ config_db/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
